ab1f9792d9b6ac6a0f508931373033b5ba11ebf0
Youngmin Yoon <yym980312@gmail.com>
2026-03-07 19:46:35 +0900

    react default js study

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -12,6 +12,13 @@ export default function SignIn() {
     console.log('email', email)
   }
 
+
+  const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+
   const data1 = '3'
 
   return (
@@ -26,7 +33,18 @@ export default function SignIn() {
         {/* if - 문장이라 표현식이아님 (script쪽에서 바인딩하거나) -> 돔에선 데이터 비교 조건같은걸로 효과는 똑같음 */}
         {/* 반복문들은 다 표현식이라 가능 */}
         {/* 삼항연산자할때 () 묶는건 여러개를 하나처럼 표시할때인데 - 하나일땐 생략해도 똑같지만 가독성을 위해 작성함 */}
+        
         {data1 === '3' && <div style={{ color: 'blue' }}>data1 is 3</div>}
+
+        <ul>
+          {products.map((i) => (
+            <li key={i.id}>
+              {i.title}
+            </li>
+          ))} 
+        </ul>
+
+
         <button className="bg-blue-500 text-white p-2 rounded-md" type="submit">SignIn</button>
       </form>
     </div>
